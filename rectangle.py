a0f1db225da414aa95914467c1555e57c4757ead
Egorov Evgeniy <egorovzhenya@gmail.com>
2022-06-27 21:04:54 +0400

    Added has_intersection() function, which takes two rectangles and returns True if they intersect

--- rectangle.py
+++ rectangle.py
@@ -9,4 +9,18 @@ class Rectangle:
     x_max: int
     y_max: int
 
+    @staticmethod
+    def has_intersection(rect1, rect2):
+        """The logic is based on Separating Axis Theorem"""
+        return any([
+            rect1.x_min < rect2.x_min < rect1.x_max,
+            rect1.x_min < rect2.x_max < rect1.x_max,
+            rect1.y_min < rect2.y_min < rect1.y_max,
+            rect1.y_min < rect2.y_max < rect1.y_max,
+        ])
 
+
+if __name__ == '__main__':
+    first_rect = Rectangle(1, 1, 5, 5)
+    second_rect = Rectangle(4, 4, 9, 9)
+    print(Rectangle.has_intersection(first_rect, second_rect))

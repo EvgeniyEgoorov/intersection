7222c13555ac8c32f5e7ee69c25926c2e24d7563
Egorov Evgeniy <egorovzhenya@gmail.com>
2022-06-27 21:12:14 +0400

    Added test_has_intersection() which checks various scenarios for rectangle module

--- test_rectangle.py
+++ test_rectangle.py
@@ -9,4 +9,14 @@ class TestRectangle(unittest.TestCase):
         self.rect3 = Rectangle(2, 2, 5, 7)
         self.rect4 = Rectangle(8, 8, 22, 44)
 
+    def test_has_intersection(self):
+        """
+        First scenario returns AssertionError if rectangles have no intersection (!= True)
+        Second scenario returns AssertionError if rectangles have intersection (!= False)
+        """
+        self.assertTrue(Rectangle.has_intersection(self.rect1, self.rect2), "No intersection")
+        self.assertFalse(Rectangle.has_intersection(self.rect3, self.rect4), "There is an intersection")
 
+
+if __name__ == '__main__':
+    unittest.main()
